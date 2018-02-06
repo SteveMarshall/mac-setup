b9d54f1e5ac3023d34eb43249737fadfa59d7f9e
Steve Marshall <steve@stevemarshall.com>
2018-02-06 13:33:58 +0000

    Use my own simpler Homebrew installer
    Norm's original homebrew installer is a bit complicated, and doesn't
    currently work, so this is one that definitely does work, is
    repeatable, and is closer to the real Homebrew installation.

---
+++ script/install_homebrew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+local -a required_dirs=(Homebrew Cellar Frameworks bin etc include lib opt sbin share var)
+
+if ! type -t brew >/dev/null; then
+    status 'Install homebrew'
+    sudo git clone git@github.com:Homebrew/brew.git "$HOMEBREW_REPOSITORY"
+    sudo ln -sf "$HOMEBREW_REPOSITORY/bin/brew" "$HOMEBREW_PREFIX/bin/brew"
+    git -C "$(brew --repo homebrew/core)" fetch --unshallow
+
+    silent_pushd $HOMEBREW_PREFIX
+    sudo mkdir -p "${required_dirs[@]}"
+    sudo chown -R $USER "${required_dirs[@]}"
+    silent_popd
+
+    brew doctor
+else
+    brew update
+    brew upgrade
+fi
--- script/install_ruby.sh
+++ script/install_ruby.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-brew_owner=`/usr/bin/stat -f %Su "$(command -v brew)"`
-if [[ $brew_owner == 'root' ]]; then
-    sudo chown -h $USER `command -v brew`
-fi
-
 ruby_version=`cat ~/.ruby-version`
 if [[ ! -d ~/.rubies/$ruby_version ]]; then
     ruby-install $ruby_version
