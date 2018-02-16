c35f8472e15f90ceda66c65135e19f44c906c5ce
Steve Marshall <steve@stevemarshall.com>
2018-02-16 10:00:05 +0000

    Add homedir et al to Finder sidebar favourites
    This used to be possible using a built-in tool that ships with macOS
    (`sfltool`), but that [seems to have been neutered in 10.13 with no
    plans to reinstate
    it](https://www.jamf.com/jamf-nation/discussions/20218/the-slftool-thread#responseChild158766).
    
    The only approach that seems to still be working is to use
    [FinderSidebarEditor](https://github.com/robperc/FinderSidebarEditor), so
    that's what I'm doing.
    
    `sidebar.add()` always pushes the listed items to the top of the list, so
    we add them in reverse order.

---
+++ script/configure_finder_favourites.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -s -O https://raw.githubusercontent.com/robperc/FinderSidebarEditor/master/FinderSidebarEditor.py
+
+python -c "
+from FinderSidebarEditor import FinderSidebar
+sidebar = FinderSidebar()
+sidebar.remove('All My Files')
+sidebar.remove('Documents')
+sidebar.add('$HOME/Applications')
+sidebar.add('/Applications')
+sidebar.add('$HOME')
+"
+
+rm -rf FinderSidebarEditor.py*
