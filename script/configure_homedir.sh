30c0adc129dbea4ebd8e6129b652ade157e1318d
Steve Marshall <steve@stevemarshall.com>
2024-11-12 12:07:20 +0000

    Move SSH and GPG files manually to avoid clashes
    If we've already installed MacGPG or used SSH, we may have files in
    these directories. This would normally cause an error, but this avoids
    that.

--- script/configure_homedir.sh
+++ script/configure_homedir.sh
@@ -5,6 +5,8 @@ pushd ~ > /dev/null
 if [[ ! -d ~/.git ]]; then
     git clone git@github.com:SteveMarshall/homedir.git
     chflags hidden ~/homedir/*
+    mv ~/homedir/.ssh/* ~/.ssh
+    mv ~/homedir/.gnupg/* ~/.gnupg
     mv ~/homedir/* ~/homedir/.[^.]* ~
     rm -df ~/homedir
 else
